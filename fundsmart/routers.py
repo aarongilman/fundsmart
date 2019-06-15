f805f727cdc1b596d67df5ef3bf51eab99df6d93
pranitaN <npranita2@gmail.com>
2019-06-15 13:05:50 +0530

    - Dashboard historical performance, pie chart and doughnut chart API

--- fundsmart/routers.py
+++ fundsmart/routers.py
@@ -2,7 +2,9 @@
 from django.urls import re_path
 from rest_framework import routers
 
-from portfolios.views import ImportPortfolioFund
+from portfolios.views import HistoricalPerformanceDifference,\
+    ImportPortfolioFund, DashboardLinePlotApi, DashboardDoughnutChart,\
+    DashboardPieChart
 from portfolios.viewsets import PortfolioViewSet, PortfolioFundViewSet,\
     SecurityViewSet
 
@@ -10,12 +12,18 @@ app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'portfolio', PortfolioViewSet, base_name='portfolio')
-router.register(r'portfolio_fund', PortfolioFundViewSet, base_name='portfolio-fund')
+router.register(r'portfolio_fund', PortfolioFundViewSet,
+                base_name='portfolio-fund')
 router.register(r'security', SecurityViewSet, base_name='security')
 
 urlpatterns = router.urls
 
 urlpatterns += [
     re_path(r'^import_portfolio_fund/$', ImportPortfolioFund.as_view(),
-            name='import-portfolio-fund')
+            name='import-portfolio-fund'),
+    re_path(r'^historical_performance_difference/$',
+            HistoricalPerformanceDifference.as_view()),
+    re_path(r'^dashboard_line_graph/$', DashboardLinePlotApi.as_view()),
+    re_path(r'^dashboard_doughnut_chart/$', DashboardDoughnutChart.as_view()),
+    re_path(r'^dashboard_pie_chart/$', DashboardPieChart.as_view())
 ]
--- portfolios/views.py
+++ portfolios/views.py
@@ -1,11 +1,13 @@
 # views.py in portfolios app
 import xlrd, logging
+
+from django.db.models import Count
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, status
 
 from .serializers import ImportPortfolioFundSerializer
-from .models import Security, Portfolio, PortfolioFund
+from .models import Security, Portfolio, PortfolioFund, FundDetail
 
 logger = logging.getLogger('fundsmart')
 
@@ -28,8 +30,8 @@ class ImportPortfolioFund(APIView):
                                      Portfolio(name="Portfolio3")])
             except Exception as e:
                 logger.error(
-                    'ImportPortfolioFund:Error {} occurred while creating portfolios'.format(
-                        e))
+                    'ImportPortfolioFund:Error {} occurred while creating\
+                    portfolios'.format(e))
             try:
                 security_isin = ws.col_values(0)
                 securities = Security.objects.filter(isin__in=security_isin)
@@ -66,3 +68,109 @@ class ImportPortfolioFund(APIView):
                     e))
             return Response("failed to add into list",
                             status.HTTP_400_BAD_REQUEST)
+
+
+class HistoricalPerformanceDifference(APIView):
+    """API view to get historical Performance difference calculation"""
+    def get(self, request):
+        fund_details = FundDetail.objects.all()
+        # Get your portfolio's funds
+        portfolio = Portfolio.objects.filter(created_by=request.user).first()
+        isin_list = PortfolioFund.objects.\
+            filter(security__asset_type='Mutual Fund', portfolio=portfolio,
+                   created_by=request.user).values_list('security__id_value',
+                                                        flat=True)
+
+        # fund details for existing funds
+        portfolio_fund_details = fund_details.filter(fund_id__in=isin_list)
+        # Annual expense calculation for existing funds
+        portfolio_annual_expense = [float(x) for x in portfolio_fund_details.
+                                    values_list('fund_exp_ratio', flat=True)]
+        portfolio_avg_annual_expense =\
+            sum(portfolio_annual_expense)/len(portfolio_annual_expense)
+
+        # 1 year, 3 year and 5 year return for existing funds
+        portfolio_1_year = [float(x) for x in portfolio_fund_details.
+                            values_list('return_1_year', flat=True)]
+        portfolio_avg_1_year = sum(portfolio_1_year)/len(portfolio_1_year)
+
+        portfolio_3_year = [float(x) for x in portfolio_fund_details.
+                            values_list('return_3_year', flat=True)]
+        portfolio_avg_3_year = sum(portfolio_3_year)/len(portfolio_3_year)
+
+        portfolio_5_year = [float(x) for x in portfolio_fund_details.
+                            values_list('return_5_year', flat=True)]
+        portfolio_avg_5_year = sum(portfolio_5_year)/len(portfolio_5_year)
+
+        # recommended 4 funds
+        recommended_funds = fund_details[:4]#.filter(for_recommendation=True)[:4]
+        # Annual expense calculation for recommended
+        recommended_annual_expense = [float(x) for x in recommended_funds.
+                                      values_list('fund_exp_ratio', flat=True)]
+        recommended_avg_annual_expense = \
+            sum(recommended_annual_expense)/len(recommended_annual_expense)
+
+        # 1 year, 3 year and 5 year for existing
+        recommended_1_year = [float(x) for x in recommended_funds.
+                              values_list('return_1_year', flat=True)]
+        recommended_avg_1_year = sum(recommended_1_year)/len(recommended_1_year)
+
+        recommended_3_year = [float(x) for x in recommended_funds.
+                              values_list('return_3_year', flat=True)]
+        recommended_avg_3_year = sum(recommended_3_year)/len(recommended_3_year)
+
+        recommended_5_year = [float(x) for x in recommended_funds.
+                              values_list('return_5_year', flat=True)]
+        recommended_avg_5_year = sum(recommended_5_year)/len(recommended_5_year)
+
+        exp_diff = recommended_avg_annual_expense - portfolio_avg_annual_expense
+        data = [{"existing": {'annual_expense': portfolio_avg_annual_expense,
+                              '1-year': portfolio_avg_1_year,
+                              '3-year': portfolio_avg_3_year,
+                              '5-year': portfolio_avg_5_year},
+                 "recommended": {
+                     'annual_expense': recommended_avg_annual_expense,
+                     '1-year': recommended_avg_1_year,
+                     '3-year': recommended_avg_3_year,
+                     '5-year': recommended_avg_5_year},
+                 "difference": {
+                     'annual_expense': exp_diff,
+                     '1-year': recommended_avg_1_year-portfolio_avg_1_year,
+                     '3-year': recommended_avg_3_year-portfolio_avg_3_year,
+                     '5-year': recommended_avg_5_year-portfolio_avg_5_year}}]
+        return Response(data, status=200)
+
+
+class DashboardLinePlotApi(APIView):
+    """API view to compare historical market values of three portfolios"""
+
+    def get(self, request):
+        data = []
+        portfolios = Portfolio.objects.filter()
+        return Response(data, status=200)
+
+
+class DashboardDoughnutChart(APIView):
+    """APIView to display analytics on user’s holding in different Industries"""
+
+    def get(self, request):
+        portfolio = Portfolio.objects.filter(created_by=request.user).first()
+        data = PortfolioFund.objects.filter(
+            portfolio=portfolio, created_by=request.user).\
+            values('security__industry').\
+            annotate(total=Count('security__industry', istinct=True)).\
+            order_by('total')
+        return Response(data, status=200)
+
+
+class DashboardPieChart(APIView):
+    """APIView to display  holdings’ asset classes in user’s portfolio"""
+
+    def get(self, request):
+        portfolio = Portfolio.objects.filter(created_by=request.user).first()
+        data = PortfolioFund.objects.filter(
+            portfolio=portfolio, created_by=request.user).\
+            values('security__asset_type').\
+            annotate(total=Count('security__asset_type', distinct=False)).\
+            order_by('total')
+        return Response(data, status=200)
