c49294328cf470dac9969b51e9477d630eb1c155
demonarola <demo.narola@gmail.com>
2019-06-29 13:01:17 +0530

    Fund Page Integration

--- frontend/FundSmart/src/app/fund/fund.component.ts
+++ frontend/FundSmart/src/app/fund/fund.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ServercommunicationService } from '../servercommunication.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
+
 
 @Component({
-  selector: 'app-fund',
-  templateUrl: './fund.component.html',
-  styleUrls: ['./fund.component.css']
+    selector: 'app-fund',
+    templateUrl: './fund.component.html',
+    styleUrls: ['./fund.component.css']
 })
 export class FundComponent implements OnInit {
+    result: any = [];
+    total = 0;
+    closeResult: string;
+    portfolio: any;
+    @ViewChild(DataTableDirective) dtElement: DataTableDirective;
+    dtTrigger = new Subject();
+   // dtOptions: DataTables.Settings = {};
 
-  constructor() { }
+    constructor(private userService: ServercommunicationService, private modalService: NgbModal
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getFunds();
+    }
 
+
+
+    getFunds() {
+        this.userService.getUserPortfolio().subscribe(
+            fundlist => {
+                this.result = fundlist;
+                console.log("result", this.result);
+            })
+    }
+
+    header_modals(modalid) {
+        this.modalService.open(modalid, {
+            ariaLabelledBy: 'app-fund',
+            windowClass: 'long-pop sign-pop', centered: true
+        }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    updatePortfolio() {
+        this.userService.update_One_Object(this.portfolio).subscribe(
+            data => {
+                console.log("data", data);
+                alert('Data Updated');
+            }
+        )
+    }
 }
--- frontend/FundSmart/src/app/servercommunication.service.ts
+++ frontend/FundSmart/src/app/servercommunication.service.ts
@@ -4,221 +4,260 @@ import { AuthService, SocialUser, GoogleLoginProvider } from "angularx-social-lo
 import { IntercomponentCommunicationService } from './intercomponent-communication.service';
 import { $ } from 'protractor';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ServercommunicationService {
-  // api_link = 'http://3.16.111.80/';
-  api_link = 'http://localhost:8000/';
-  // api_link = 'http://192.168.100.111:8000/';
+    // api_link = 'http://3.16.111.80/';
+    // api_link = 'http://localhost:8000/';
+    api_link = 'http://192.168.100.111:8000/';
 
-  // reglink = this.api_link + 'rest-auth/registration/';
-  // login_link = this.api_link + 'rest-auth/login/';
-  socialuser: SocialUser;
-  currentuser: any;
-  httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' });
-  getHttpheader = new HttpHeaders({ 'Content-type': 'application/json' });
-  count = 0;
-  constructor(private http: HttpClient, private authService: AuthService,
-    private interconn: IntercomponentCommunicationService) { }
-  userkey: string;
+    // reglink = this.api_link + 'rest-auth/registration/';
+    // login_link = this.api_link + 'rest-auth/login/';
+    socialuser: SocialUser;
+    currentuser: any;
+    httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' });
+    getHttpheader = new HttpHeaders({ 'Content-type': 'application/json' });
+    count = 0;
+    constructor(private http: HttpClient, private authService: AuthService,
+        private interconn: IntercomponentCommunicationService) { }
+    userkey: string;
 
-  doRegistration(body) {
-    // // console.log(obj);
-    return this.http.post(this.api_link + 'rest-auth/registration/', body, { headers: this.httpHeaders });
+    doRegistration(body) {
+        // // console.log(obj);
+        return this.http.post(this.api_link + 'rest-auth/registration/', body, { headers: this.httpHeaders });
 
-  }
-
-  doLogin(body) {
-    return this.http.post(this.api_link + 'rest-auth/login/', body, { headers: this.httpHeaders });
-  }
-
-  socialLogin(user) {
-    this.socialuser = user;
-    const body = { access_token: user.authToken };
-    this.http.post(this.api_link + 'rest-auth/google/', body, { headers: this.httpHeaders }).subscribe(data => {
-      // console.log(data);
-      localStorage.setItem('authkey', data['key']);
-      this.getUser(data['key']);
-    });
-
-  }
-
-  getUser(key: string) {
-    this.userkey = key;
-    this.http.get(this.api_link + 'rest-auth/user/', {
-      headers: new HttpHeaders({ Authorization: 'Token ' + key })
-    }).subscribe(userdata => {
-      this.currentuser = userdata;
-      this.interconn.callComponentMethod();
-    });
-  }
-
-  change_password(oldpass: string, newpass: string, confirmpass: string) {
-    const body = {
-      old_password: oldpass,
-      new_password1: newpass,
-      new_password2: confirmpass
-    };
-    return this.http.post(this.api_link + 'rest-auth/password/change/', body, {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
-
-  reset_pwd_sendemail(emailid: string) {
-    const body = { email: emailid };
-    return this.http.post(this.api_link + 'rest-auth/password/reset/', body, { headers: this.httpHeaders });
-  }
-
-  resetpassword_req(userid: string, usertoken: string, pass1: string, pass2: string) {
-    const body = {
-      uid: userid,
-      token: usertoken,
-      new_password1: pass1,
-      new_password2: pass2
-    };
-    return this.http.post(this.api_link + 'rest-auth/password/reset/confirm/', body, { headers: this.httpHeaders });
-  }
-
-  update_User(user: any) {
-    const body = {
-      username: user.username,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      phone_number: user.phone_number
-    };
-    return this.http.put(this.api_link + 'rest-auth/user/',
-      body, { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
-  }
-
-  uploadfile(file) {
-
-    // const body = { data_file: file };
-    return this.http.post(this.api_link + 'api/import_portfolio_fund/', file, {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
-
-  get_portfolio_fund() {
-    if (this.userkey) {
-      return this.http.get(this.api_link + 'api/portfolio_fund/', {
-        headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-      });
     }
-  }
 
-  get_security() {
-    return this.http.get(this.api_link + 'api/security/', { headers: this.httpHeaders });
-  }
-
-  get_home_pie_chart() {
-    return this.http.get(this.api_link + 'api/dashboard_pie_chart/', {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
-
-  get_deshboard_doughnut_chart() {
-    return this.http.get(this.api_link + 'api/dashboard_doughnut_chart/', {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
-
-  get_historical_perfomance() {
-    return this.http.get(this.api_link + 'api/historical_performance_difference/', {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
-
-
-
-
-  add_portfolio_fund(fquantity, userportfolio, selectedsecurity, createdby) {
-    const body = { quantity: fquantity, portfolio: userportfolio, security: selectedsecurity, created_by: createdby };
-    return this.http.post(this.api_link + 'api/portfolio_fund/', body,
-      { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
-  }
-
-  getUserPortfolio() {
-    return this.http.get(this.api_link + 'api/portfolio/', { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
-  }
-
-  checklogin() {
-    var userkey = null;
-    userkey = localStorage.getItem('authkey');
-    // alert(userkey);
-    if (userkey != null) {
-      // alert('Came for login');
-      this.getUser(userkey);
+    doLogin(body) {
+        return this.http.post(this.api_link + 'rest-auth/login/', body, { headers: this.httpHeaders });
     }
-  }
 
-  logout() {
-    localStorage.clear();
-    this.http.post(this.api_link + 'rest-auth/logout/',
-      { headers: this.httpHeaders }).subscribe(
-        data => {
-          this.userkey = null;
-          this.currentuser = undefined;
-          this.interconn.afterlogout();
+    socialLogin(user) {
+        this.socialuser = user;
+        const body = { access_token: user.authToken };
+        this.http.post(this.api_link + 'rest-auth/google/', body, { headers: this.httpHeaders }).subscribe(data => {
+            // console.log(data);
+            localStorage.setItem('authkey', data['key']);
+            this.getUser(data['key']);
+        });
+
+    }
+
+    getUser(key: string) {
+        this.userkey = key;
+        this.http.get(this.api_link + 'rest-auth/user/', {
+            headers: new HttpHeaders({ Authorization: 'Token ' + key })
+        }).subscribe(userdata => {
+            this.currentuser = userdata;
+            this.interconn.callComponentMethod();
+        });
+    }
+
+    change_password(oldpass: string, newpass: string, confirmpass: string) {
+        const body = {
+            old_password: oldpass,
+            new_password1: newpass,
+            new_password2: confirmpass
+        };
+        return this.http.post(this.api_link + 'rest-auth/password/change/', body, {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+    reset_pwd_sendemail(emailid: string) {
+        const body = { email: emailid };
+        return this.http.post(this.api_link + 'rest-auth/password/reset/', body, { headers: this.httpHeaders });
+    }
+
+    resetpassword_req(userid: string, usertoken: string, pass1: string, pass2: string) {
+        const body = {
+            uid: userid,
+            token: usertoken,
+            new_password1: pass1,
+            new_password2: pass2
+        };
+        return this.http.post(this.api_link + 'rest-auth/password/reset/confirm/', body, { headers: this.httpHeaders });
+    }
+
+    update_User(user: any) {
+        const body = {
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            phone_number: user.phone_number
+        };
+        return this.http.put(this.api_link + 'rest-auth/user/',
+            body, { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+
+    uploadfile(file) {
+
+        // const body = { data_file: file };
+        return this.http.post(this.api_link + 'api/import_portfolio_fund/', file, {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+    get_portfolio_fund() {
+        if (this.userkey) {
+            return this.http.get(this.api_link + 'api/portfolio_fund/', {
+                headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+            });
         }
-      );
-  }
-  confirm_email(verificationkey) {
-    const body = { key: verificationkey };
-    return this.http.post(this.api_link + 'rest-auth/registration/verify-email/', body, { headers: this.httpHeaders });
-
-  }
-
-  createportfolio(number) {
-    var portfolioname = '';
-    if (this.count < 4) {
-      if (number === 1) {
-        portfolioname = 'portfolio1';
-        this.count++;
-      }
-      if (number === 2) {
-        portfolioname = 'portfolio2';
-        this.count++;
-      }
-      if (number === 3) {
-        portfolioname = 'portfolio3';
-        this.count++;
-      }
     }
 
-    const body = { name: portfolioname, created_by: this.currentuser.id };
-    return this.http.post(this.api_link + 'api/portfolio/', body,
-      { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    get_Fund() {
 
-  }
+        if (this.userkey) {
+            return this.http.get(this.api_link + 'api/portfolio/', {
+                headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+            });
+        }
+    }
 
-  updateportfoliofund(recid, fquantity, userportfolio, selectedsecurity, updatedby) {
-    const body = {
-      id: recid, quantity: Number.parseFloat(fquantity), portfolio: userportfolio,
-      security: selectedsecurity, updated_by: updatedby, created_by: this.currentuser.id
-    };
-    return this.http.put(this.api_link + 'api/portfolio_fund/' + recid + '/', body,
-      { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
-  }
+    get_One_Object() {
+        if (this.userkey) {
+            return this.http.get(this.api_link + 'api/portfolio/id_here', {
+                headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+            });
+        }
+    }
 
-  get_lineplot_chart() {
-    return this.http.get(this.api_link + 'api/dashboard_line_graph/', {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
+    post_create_fund() {
+        const body = { created_by: this.currentuser.id };
+        return this.http.post(this.api_link + 'api/portfolio/', body,
+            { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
 
-  postPrice(fundid, fundprice) {
-    const body = { id: fundid, price: fundprice };
-    return this.http.post(this.api_link + 'api/holding_detail/', body, {
-      headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
-    });
-  }
+    update_One_Object(fund: any) {
+        const body = {
+            created_at: fund.created_at,
+            created_by: fund.created_by,
+            description: fund.description,
+            id: fund.id,
+            location: fund.location,
+            marginal_tax_range: fund.marginal_tax_range,
+            name: fund.name,
+            owner_1: fund.owner_1,
+            owner_2: fund.owner_2
+        }
+        return this.http.put(this.api_link + 'api/portfolio/id_here',
+            body, { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+
+    get_security() {
+        return this.http.get(this.api_link + 'api/security/', { headers: this.httpHeaders });
+    }
+
+    get_home_pie_chart() {
+        return this.http.get(this.api_link + 'api/dashboard_pie_chart/', {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+    get_deshboard_doughnut_chart() {
+        return this.http.get(this.api_link + 'api/dashboard_doughnut_chart/', {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+    get_historical_perfomance() {
+        return this.http.get(this.api_link + 'api/historical_performance_difference/', {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
 
 
-  getHoldingDetails() {
-    return this.http.get(this.api_link + 'api/holding_detail/',
-      { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
-  }
-  // production api ----->3.16.111.80
 
-  // 3.16.111.80 server
+
+    add_portfolio_fund(fquantity, userportfolio, selectedsecurity, createdby) {
+        const body = { quantity: fquantity, portfolio: userportfolio, security: selectedsecurity, created_by: createdby };
+        return this.http.post(this.api_link + 'api/portfolio_fund/', body,
+            { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+
+    getUserPortfolio() {
+        return this.http.get(this.api_link + 'api/portfolio/', { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+
+    checklogin() {
+        var userkey = null;
+        userkey = localStorage.getItem('authkey');
+        // alert(userkey);
+        if (userkey != null) {
+            // alert('Came for login');
+            this.getUser(userkey);
+        }
+    }
+
+    logout() {
+        localStorage.clear();
+        this.http.post(this.api_link + 'rest-auth/logout/',
+            { headers: this.httpHeaders }).subscribe(
+                data => {
+                    this.userkey = null;
+                    this.currentuser = undefined;
+                    this.interconn.afterlogout();
+                }
+            );
+    }
+    confirm_email(verificationkey) {
+        const body = { key: verificationkey };
+        return this.http.post(this.api_link + 'rest-auth/registration/verify-email/', body, { headers: this.httpHeaders });
+
+    }
+
+    createportfolio(number) {
+        var portfolioname = '';
+        if (this.count < 4) {
+            if (number === 1) {
+                portfolioname = 'portfolio1';
+                this.count++;
+            }
+            if (number === 2) {
+                portfolioname = 'portfolio2';
+                this.count++;
+            }
+            if (number === 3) {
+                portfolioname = 'portfolio3';
+                this.count++;
+            }
+        }
+
+        const body = { name: portfolioname, created_by: this.currentuser.id };
+        return this.http.post(this.api_link + 'api/portfolio/', body,
+            { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+
+    }
+
+    updateportfoliofund(recid, fquantity, userportfolio, selectedsecurity, updatedby) {
+        const body = {
+            id: recid, quantity: Number.parseFloat(fquantity), portfolio: userportfolio,
+            security: selectedsecurity, updated_by: updatedby, created_by: this.currentuser.id
+        };
+        return this.http.put(this.api_link + 'api/portfolio_fund/' + recid + '/', body,
+            { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+
+    get_lineplot_chart() {
+        return this.http.get(this.api_link + 'api/dashboard_line_graph/', {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+    postPrice(fundid, fundprice) {
+        const body = { id: fundid, price: fundprice };
+        return this.http.post(this.api_link + 'api/holding_detail/', body, {
+            headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey })
+        });
+    }
+
+
+    getHoldingDetails() {
+        return this.http.get(this.api_link + 'api/holding_detail/',
+            { headers: new HttpHeaders({ Authorization: 'Token ' + this.userkey }) });
+    }
+    // production api ----->3.16.111.80
+
+    // 3.16.111.80 server
 }
