ed4adec1b5ea855dbd147d29d23477f4020f85a5
pranitaN <npranita2@gmail.com>
2019-07-11 14:17:56 +0530

    -Dashboard historical performance difference API

---
+++ portfolios/common.py
@@ -0,0 +1,15 @@
+from .models import FundHolding
+
+
+def get_quantity(quantity, security, aum, rate, price):
+    """function to get quantity"""
+    a = float(quantity.replace("%", "")) * 1000000
+    if security.asset_type == 'Mutual Fund':
+        b = float(aum * 1000000 * (1 / rate))
+        aum = a / b
+        holdings = FundHolding.objects.values_list('quantity', flat=True)\
+            .filter(fund_id=security.id_value)
+        quantity = aum * sum(holdings)
+    else:
+        quantity = a / float(price)
+    return quantity
